42f519e0cfe1048487093d982c7e4a3466148153
Timothy Robinson <timmyrobby@gmail.com>
2017-05-10 12:11:09 +0100

    Adds decrease function

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -1,12 +1,23 @@
+'use strict';
+
 describe("Thermostat", function() {
+  var thermostat;
+
+  beforeEach(function() {
+     thermostat = new Thermostat();
+  });
+
   it("has a default temperature", function() {
-    var thermostat = new Thermostat();
     expect(thermostat.temperature).toEqual(20);
   });
 
   it("increases the temperature by 1", function() {
-    var thermostat = new Thermostat();
     thermostat.increase();
     expect(thermostat.temperature).toEqual(21);
+  });
+
+  it("decreases the temperature by 1", function() {
+    thermostat.decrease();
+    expect(thermostat.temperature).toEqual(19);
   });
 });
